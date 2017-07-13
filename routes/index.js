e21347903088f130646e4622eab743adde624a1c
William Bolton <william.ellet@gmail.com>
2017-07-12 23:39:20 -0400

    Set .env and rename file path

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var septa_client = require('../app_javascripts/septa_client')
+var septa_client = require('../javascripts/septa_client')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
